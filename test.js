e34a08334b1054332527e869fc6083ced092a9d9
MarkLee <1643565650@qq.com>
2022-03-17 21:12:00 +0800

    修改注释

--- test.js
+++ test.js
@@ -13,28 +13,29 @@ let n = 0;
 let timerSpeed = 150;
 
 // -------------------------------------------
-// 输出页面上半部分的文字
+// 页面的渲染
 let print = setInterval(() => {
   n = n + 1;
   if (n > cssString.length) {
     clearInterval(print);
     return;
   }
-  //   输出从第0位到第n-1位，第n位是不会输出的
-  textPart.innerText = cssString.substring(0, n);
 
-  //   textPart滚动最底部，关键要在这里面执行
+  // 形成页面上半部分的代码文字
+  textPart.innerText = cssString.substring(0, n); // 输出从第0位到第n-1位，第n位是不会输出的
+
+  // 让页面的上半部分滚动最底部（关键要在这里面执行）
+  textPart.scrollTop = textPart.scrollHeight;
   // textPart.scrollIntoView(false);
   // textPart.scrollTop = 9999999;
-  textPart.scrollTop = textPart.scrollHeight;
 
-  //优化代码，让渲染部分跟文字部分都处于同一个计时器
+  // 形成页面的下半部分皮卡丘
   cssPart.innerHTML = cssString.substring(0, n);
 }, timerSpeed);
 
 // -------------------------------------------
-// 渲染下半部分的皮卡丘
-let renderTextAndHtml = function () {
+// print功能放入函数中，方便后面再次调用
+let renderTextAndHtmlFn = function () {
   n = n + 1;
 
   console.log(n);
@@ -51,7 +52,8 @@ let renderTextAndHtml = function () {
 // 几个功能集成到对象中
 const player = {
   runTimer() {
-    return setInterval(renderTextAndHtml, timerSpeed);
+    // setInterval会执行函数，也就是下面的renderTextAndHtmlFn
+    return setInterval(renderTextAndHtmlFn, timerSpeed);
   },
 
   pauseTimer() {
